59b63c082a998a747d02ebae4032769bd152a776
Zhouyuan Li <lizhouyuandream@gmail.com>
2014-05-02 01:06:28 -0700

    Initial commit

---
+++ obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o.d
@@ -0,0 +1,151 @@
+obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o: jni/jni_part.cpp \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ ../../sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ ../../sdk/native/jni/include/opencv2/core/core_c.h \
+ ../../sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ ../../sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ ../../sdk/native/jni/include/opencv2/flann/defines.h \
+ ../../sdk/native/jni/include/opencv2/flann/config.h
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+C:/old/android/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+C:/old/android/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+../../sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+../../sdk/native/jni/include/opencv2/core/core_c.h:
+
+../../sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+../../sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+../../sdk/native/jni/include/opencv2/flann/defines.h:
+
+../../sdk/native/jni/include/opencv2/flann/config.h:
